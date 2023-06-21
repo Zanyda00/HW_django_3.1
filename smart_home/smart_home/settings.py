533f451b77a609385a239d80ea570f54f66a8f3a
Igor Stepurko <stepurkovaychok@gmail.com>
2023-06-21 20:28:50 +0300

    bag fix

--- smart_home/smart_home/settings.py
+++ smart_home/smart_home/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'frfltvbzyfer1996',
+        'PASSWORD': 'postgres',
     }
 }
 
